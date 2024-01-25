e649f2183ddf19894b53188b44e512a1c81bb4d0
partoo <partoo@163.com>
2024-01-25 21:46:18 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # uniapp-mac
-A temporary method for running UniApp under MacOS
+HBuilderX 在 MacOS 频频吞噬内存,比如[这里](https://ask.dcloud.net.cn/question/184154?item_id=264560&rf=false) 此外，比起成熟的WebStorm、VSCode而言，体验更无须多言。从[这里]https://juejin.cn/post/7233572338968526903 搬了这个脚本来，现实中使用UniApp的场景也不多，算是备忘。
+# Usage
+1. 修改 ws.sh 中的 HBUILDER_DIR和WEIXIN_DEV_DIR两个路径，路径的末尾不要带斜杠；
+2. 编辑HbuilderX安装目录下的plugins\uniapp-cli-vite\package.json文件，在scripts 节点下增加一行:"dev:weixin": "uni -p mp-weixin"
+3. 配置Webstorm, 设置-插件：安装WeChat mini program support, 设置-语言和框架-WeChat Mini Program：小程序支持 => 启用
+4. 打开终端， ./ws.sh
---
+++ ws.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ==============根据实际修改如下两个环境变量==============
+export HBUILDER_DIR=/Applications/HBuilderX
+export WEIXIN_DEV_DIR="/Applications/wechatwebdevtools.app"
+# ==============注意变量尾部不要带斜杠==============
+
+export UNI_INPUT_DIR=$(pwd)
+export UNI_OUTPUT_DIR=$UNI_INPUT_DIR/unpackage/dist/dev/mp-weixin
+export VITE_ROOT_DIR=$UNI_INPUT_DIR
+
+export UNI_CLI_CONTEXT=$HBUILDER_DIR/plugins/uniapp-cli-vite
+export UNI_HBUILDERX_PLUGINS=$HBUILDER_DIR/plugins
+export UNI_NPM_DIR=$HBUILDER_DIR/plugins/npm
+export UNI_NODE_DIR=$HBUILDER_DIR/plugins/node
+
+export UNI_COMPILER_VERSION_TYPE=r
+export UNI_NODE_ENV=development
+export UNI_UTS_PLATFORM=mp-weixin
+export UNI_HBUILDERX_LANGID=zh_CN
+export UNI_PLATFORM=mp-weixin
+export VITE_USER_NODE_ENV=development
+export NODE_ENV=development
+export NODE_SKIP_PLATFORM_CHECK=1
+
+export NODE="$UNI_NODE_DIR/node"
+
+cd "$UNI_CLI_CONTEXT" || exit
+clear
+
+open -a "$WEIXIN_DEV_DIR" --args -o "$UNI_OUTPUT_DIR"
+
+"$NODE" --max-old-space-size=2048 --no-warnings "$UNI_NPM_DIR/node_modules/npm/bin/npm-cli.js" run dev:weixin
